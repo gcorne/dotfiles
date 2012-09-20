71f7aa4f466c2e008affb011118247f8b128bf16
Gregory Cornelius <gcorne@gmail.com>
2012-09-19 22:36:32 -0400

    Add a simple install script.

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Simple script that creates the various symlinks
+
+ln -s $HOME/.dotfiles/.vim $HOME/.vim
+ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
+ln -s $HOME/.dotfiles/.gvimrc $HOME/.gvimrc
+ln -s $HOME/.dotfiles/.scvimrc $HOME/.scvimrc
+ln -s $HOME/.dotfiles/.scvim $HOME/.scvim
+
+ln -s $HOME/.dotfiles/.ctags $HOME/.ctags
+ln -s $HOME/.dotfiles/.bash_profile $HOME/.bash_profile
+ln -s $HOME/.dotfiles/.screenrc $HOME/.screenrc
