cb26f2bb757ffc4de2c54052da8d051234fb81cf
Gregory Cornelius <gcorne@bu.edu>
2012-10-10 14:09:39 -0400

    Modify install script to handle storing dotfiles in any directory.

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Simple script that creates the various symlinks
 
-ln -s $HOME/.dotfiles/.vim $HOME/.vim
-ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
-ln -s $HOME/.dotfiles/.gvimrc $HOME/.gvimrc
-ln -s $HOME/.dotfiles/.scvimrc $HOME/.scvimrc
-ln -s $HOME/.dotfiles/.scvim $HOME/.scvim
+ln -s $DIR/.vim $HOME/.vim
+ln -s $DIR/.vimrc $HOME/.vimrc
+ln -s $DIR/.gvimrc $HOME/.gvimrc
+ln -s $DIR/.scvimrc $HOME/.scvimrc
+ln -s $DIR/.scvim $HOME/.scvim
 
-ln -s $HOME/.dotfiles/.ctags $HOME/.ctags
-ln -s $HOME/.dotfiles/.bash_profile $HOME/.bash_profile
-ln -s $HOME/.dotfiles/.screenrc $HOME/.screenrc
+ln -s $DIR/.ctags $HOME/.ctags
+ln -s $DIR/.bash_profile $HOME/.bash_profile
+ln -s $DIR/.screenrc $HOME/.screenrc
